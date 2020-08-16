0528ac157b2d7c15f6a3202f910b9d7157ecdd74
ksj <ksj_1016@naver.com>
2020-08-16 15:47:01 +0900

    gpg test2
    gpg test2

--- src/main/java/test/HibernateTest.java
+++ src/main/java/test/HibernateTest.java
@@ -8,27 +8,51 @@ import util.HibernateUtil;
 
 public class HibernateTest {
     public static void main(String[] args) {
-        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-        Session session = sessionFactory.openSession();
-
-        session.beginTransaction();
-
-        RegisteredProduct registeredProduct = new RegisteredProduct();
-        registeredProduct.setId(1);
-        registeredProduct.setName("반팔 티셔츠");
-        registeredProduct.setBrand("a사");
-        registeredProduct.setPrice(20000);
-        registeredProduct.setQuantity(100);
-
-        session.save(registeredProduct);
 
         RegisteredProducts registeredProducts = RegisteredProducts.getInstance();
 
-        RegisteredProduct registeredProduct1 = registeredProducts.getProduct(1);
-        System.out.println(registeredProduct1.getName());
+        //RegisteredProduct registeredProduct1 = registeredProducts.getProduct(1);
+        RegisteredProduct registeredProduct1 = registeredProducts.searchProduct("name", "신발");
+        System.out.println(registeredProduct1);
 
 
-        session.getTransaction().commit();
+//        try (SessionFactory sessionFactory = HibernateUtil.getSessionFactory()) {
+//            Session session = HibernateUtil.getSession();
+//
+//            session.beginTransaction();
+//
+//            RegisteredProduct registeredProduct = new RegisteredProduct(1, "반팔 티셔츠", "A사", 20000, 100);
+//            session.save(registeredProduct);
+//            registeredProduct = new RegisteredProduct(2, "컴퓨터", "B사", 1000000, 30);
+//            session.save(registeredProduct);
+//            registeredProduct = new RegisteredProduct(3, "책", "C사", 15000, 150);
+//            session.save(registeredProduct);
+//            registeredProduct = new RegisteredProduct(4, "가죽자켓", "D사", 20000, 70);
+//            session.save(registeredProduct);
+//            registeredProduct = new RegisteredProduct(5, "장난감", "E사", 5000, 50);
+//            session.save(registeredProduct);
+//            registeredProduct = new RegisteredProduct(6, "화장품", "F사", 40000, 100);
+//            session.save(registeredProduct);
+//            registeredProduct = new RegisteredProduct(7, "신발", "G사", 250000, 5);
+//            session.save(registeredProduct);
+//
+//
+//            session.getTransaction().commit();
+//
+//            RegisteredProducts registeredProducts = RegisteredProducts.getInstance();
+//
+//            //RegisteredProduct registeredProduct1 = registeredProducts.getProduct(1);
+//            RegisteredProduct registeredProduct1 = registeredProducts.searchProduct("name", "신발");
+//            System.out.println(registeredProduct1);
+//
+
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+
+        //session.getTransaction().commit();
+        HibernateUtil.getSession().close();
         HibernateUtil.getSessionFactory().close();
     }
 }
