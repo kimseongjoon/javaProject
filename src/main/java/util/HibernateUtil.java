df828b85bfc30821560570ce69605782802de006
ksj <ksj_1016@naver.com>
2020-08-16 15:51:56 +0900

    push test
    push test

--- src/main/java/util/HibernateUtil.java
+++ src/main/java/util/HibernateUtil.java
@@ -6,24 +6,30 @@ import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
     private static SessionFactory sessionFactory;
-     private  static SessionFactory buildSessionFactory() {
-         try {
-             Configuration configuration = new Configuration();
-             configuration.configure("hibernate.cfg.xml");
-             return configuration.buildSessionFactory();
-         } catch (Throwable e) {
-             e.printStackTrace();
-             throw new ExceptionInInitializerError(e);
-         }
-     }
+    private static Session session;
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure("hibernate.cfg.xml");
+            return configuration.buildSessionFactory();
+        } catch (Throwable e) {
+            e.printStackTrace();
+            throw new ExceptionInInitializerError(e);
+        }
+    }
 
     public static SessionFactory getSessionFactory() {
-        if(sessionFactory == null) sessionFactory = buildSessionFactory();
+        if (sessionFactory == null) sessionFactory = buildSessionFactory();
         return sessionFactory;
     }
 
     public static Session getSession() {
-        if(sessionFactory == null) sessionFactory = buildSessionFactory();
+        if (session == null) {
+            if (sessionFactory == null) sessionFactory = buildSessionFactory();
+            session = sessionFactory.openSession();
+        }
+
         return sessionFactory.openSession();
     }
 }
